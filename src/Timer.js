8fadee88f4e6861776e087a01f67095d2e641da7
khwilo <khwilowatai@gmail.com>
2019-06-12 13:17:22 +0300

    Create the Timer component

---
+++ src/Timer.js
@@ -0,0 +1,35 @@
+import React, { Component } from 'react';
+
+class Timer extends Component {
+    render() {
+        const elapsedString = this.props.elapsed;
+        return (
+            <div className="card mt-2">
+                <div className="card-body">
+                    <div className="card-title h3">
+                        {this.props.title}
+                    </div>
+                    <div className="card-subtitle text-muted">
+                        {this.props.project}
+                    </div>
+                    <div className="d-flex justify-content-center align-items-center mt-2">
+                        <h2>{elapsedString}</h2>
+                    </div>
+                    <div className="extra content d-flex">
+                        <span className="ml-auto edit-btn">
+                            <i className="fa fa-pencil-square-o" aria-hidden="true" />
+                        </span>
+                        <span className="ml-2 delete-btn">
+                            <i className="fa fa-trash" aria-hidden="true" />
+                        </span>
+                    </div>
+                </div>
+                <div className="card-footer text-center bg-light text-info start-btn">
+                    Start
+                </div>
+            </div>
+        );
+    }
+}
+
+export default Timer;
