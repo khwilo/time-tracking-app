511a02ece22bc6b8a078e4e5fc9d514803533c61
khwilo <khwilowatai@gmail.com>
2019-06-12 13:20:50 +0300

    Render the TimerForm if editFormOpen prop is true or else render the Timer

--- src/EditableTimer.js
+++ src/EditableTimer.js
@@ -1,13 +1,25 @@
 import React, { Component } from 'react';
+import Timer from './Timer';
 import TimerForm from './TimerForm';
 
 class EditableTimer extends Component {
     render() {
-        return (
-            <TimerForm
-                title   = {this.props.title}
-                project = {this.props.project} />
-        );
+        if (this.props.editFormOpen) {
+            return (
+                <TimerForm
+                    title   = {this.props.title}
+                    project = {this.props.project}
+                />
+            );
+        } else {
+            return (
+                <Timer
+                    title        = {this.props.title}
+                    project      = {this.props.project}
+                    elapsed      = {this.props.elapsed}
+                    runningSince = {this.props.runningSince} />
+            );
+        }
     }
 }
 
