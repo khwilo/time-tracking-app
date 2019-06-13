148ba0f595afb3412c2ea6b7df6042b7ddccd688
khwilo <khwilowatai@gmail.com>
2019-06-13 09:15:29 +0300

    Add state to the TimerDashboard and receive the props in the EditableTimerList

--- src/TimerDashboard.js
+++ src/TimerDashboard.js
@@ -1,13 +1,36 @@
 import React, { Component } from 'react';
+import uuid from 'uuid';
+
 import EditableTimerList from './EditableTimerList';
 import ToggleableTimerForm from './ToggleableTimerForm';
 
 class TimerDashboard extends Component {
+  state = {
+    timers: [
+      {
+        title       : "Practice squat",
+        project     : "Gym Chores",
+        id          : uuid.v4(),
+        elapsed     : 5456099,
+        runningSince: Date.now(),
+      },
+      {
+        title       : "Bake squash",
+        project     : "Kitchen Chores",
+        id          : uuid.v4(),
+        elapsed     : 1273998,
+        runningSince: null,
+      }
+    ],
+  };
+
   render() {
     return (
       <div className="d-flex justify-content-center">
         <div>
-          <EditableTimerList />
+          <EditableTimerList
+            timers = {this.state.timers}
+          />
           <ToggleableTimerForm
             isOpen = {true}
           />
