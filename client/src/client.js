881127c7375145af8ca0e90a95168358f10e8453
khwilo <khwilowatai@gmail.com>
2019-06-21 14:58:09 +0300

    Implement the logic to interact with the server

---
+++ client/src/client.js
@@ -0,0 +1,82 @@
+export const getTimers = (success) => {
+    return fetch('/api/timers', {
+        headers: {
+            'Accept': 'application/json',
+        },
+    }).then(checkStatus)
+        .then(parseJson)
+        .then(success);
+}
+
+export const createTimer = (data) => {
+    return fetch('/api/timers', {
+        method : 'POST',
+        headers: {
+            'Accept'      : 'application/json',
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(data),
+    }).then(checkStatus);
+}
+
+export const updateTimer = (data) => {
+    return fetch('/api/timers', {
+        method : 'PUT',
+        headers: {
+            'Accept'      : 'application/json',
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(data),
+    }).then(checkStatus);
+}
+
+export const deleteTimer = (data) => {
+    return fetch('/api/timers', {
+        method : 'DELETE',
+        headers: {
+            'Accept'      : 'application/json',
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(data)
+    }).then(checkStatus);
+}
+
+export const startTimer = (data) => {
+    return fetch('/api/timers/start', {
+        method : 'POST',
+        headers: {
+            'Accept'      : 'application/json',
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(data),
+    }).then(checkStatus);
+}
+
+export const stopTimer = (data) => {
+    return fetch('/api/timers/stop', {
+        method : 'POST',
+        headers: {
+            'Accept'      : 'application/json',
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(data),
+    }).then(checkStatus);
+}
+
+const checkStatus = (response) => {
+    if (response.status >= 200 && response.status < 300) {
+        return response;
+    } else {
+        const error = new Error(`Http Erro ${response.statusText}`);
+
+        error.status   = response.statusText;
+        error.response = response;
+
+        console.log(error);
+        throw error;
+    }
+}
+
+const parseJson = (response) => {
+    return response.json();
+}
