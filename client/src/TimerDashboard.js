b3ca0d26b1c613c102a2567df6d7c7e14715721c
khwilo <khwilowatai@gmail.com>
2019-06-21 14:58:45 +0300

    Fetch the initial data from the server and render

--- client/src/TimerDashboard.js
+++ client/src/TimerDashboard.js
@@ -4,25 +4,23 @@ import uuid from 'uuid';
 import EditableTimerList from './EditableTimerList';
 import ToggleableTimerForm from './ToggleableTimerForm';
 
+import { getTimers } from './client';
+
 class TimerDashboard extends Component {
   state = {
-    timers: [
-      {
-        title       : "Practice squat",
-        project     : "Gym Chores",
-        id          : uuid.v4(),
-        elapsed     : 5456099,
-        runningSince: Date.now(),
-      },
-      {
-        title       : "Bake squash",
-        project     : "Kitchen Chores",
-        id          : uuid.v4(),
-        elapsed     : 1273998,
-        runningSince: null,
-      }
-    ],
+    timers: [],
   };
+
+  componentDidMount() {
+    this.loadTimersFromServer();
+    setInterval(this.loadTimersFromServer, 5000);
+  }
+
+  loadTimersFromServer = () => {
+    getTimers((serverTimers) => {
+      this.setState({ timers: serverTimers })
+    });
+  }
 
   handleCreateFormSubmit = (timer) => {
     this.createTimer(timer);
